e48980733a799a662f9d339bd705d1775758ba4e
skarb <Him1897@naver.com>
2024-02-15 01:58:42 +0900

    2024-02-14 Icon and Text Applications testing

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-/*
+  /*
     return MaterialApp(
       home: Scaffold( // 상중하 나눠주는 위젯
         appBar: AppBar(title: Text('김남규 앱임'), backgroundColor: Colors.blue),
@@ -51,10 +51,11 @@ class MyApp extends StatelessWidget {
       )
     );
     */ // 3강의
-    // 4강의
+  /*
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar( title: Text('김남규 앱임'), backgroundColor: Colors.blue),
+        appBar: AppBar(actions: [Icon(Icons.search)] , title: Text('검색'), backgroundColor: Colors.blue ),
+        //appBar: AppBar( title: Text('김남규 앱임'), backgroundColor: Colors.blue),
         body: ElevatedButton(
           onPressed: (){},
           child: SizedBox(
@@ -62,8 +63,106 @@ class MyApp extends StatelessWidget {
             height: 50,
             child: Center(child: Text('버튼')),
           ),
-        )
-      )
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(Icons.phone),
+              Icon(Icons.message),
+              Icon(Icons.home)
+            ],
+          )
+          ),
+        ),
+    );
+    */ // 4강의
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(actions: [
+          Icon(Icons.search),
+          Icon(Icons.read_more),
+          Icon(Icons.doorbell) ],
+          title: Text('금호동3가'),
+          backgroundColor: Colors.blue,
+        ),
+        body: SizedBox(
+          width: double.infinity,
+          height: 150,
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.red)
+            ),
+            child: Row(
+              children: [
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.blue)
+                        ),
+                        child: Image.asset('gam.JPG'),
+                        height: 300,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.yellow)
+                        ),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.max,
+                          crossAxisAlignment: CrossAxisAlignment.start, // 컬럼 안의 텍스트 좌측정렬
+                          children: [
+                            SizedBox(
+                              child: Text('피큐어',
+                              style: TextStyle(color: Colors.red),
+                              ),
+                            ),
+                            SizedBox(child:
+                              Text('일본 직수입, 건담 피규어(사람 크기)',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold, // 폰트 굵게
+                                  letterSpacing: 2.0, // 자간 간격
+                                ),
+                              ),
+                            ),
+                            SizedBox(child:
+                              Text('[일본-후쿠오카 캐널시티] : 5분 전',
+                                style: TextStyle(
+                                  color: Colors.grey
+                                ),
+                              )
+                            ),
+                            SizedBox(child:
+                              Text('50,000원',
+                                style: TextStyle(
+                                  fontSize: 15
+                                ),
+                              )
+                            ),
+                            Container(child:
+                              Icon(Icons.favorite_border),
+                              //padding: const EdgeInsets.all(3.50),
+                              //margin: const EdgeInsets.all(20),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
 
   }
